e40a9ad511fd55d17712ab9c15565a1965df2e22
Wilman Vasquez <wilman.vasquez@koombea.com>
2022-01-31 14:18:52 -0500

    Show menu

--- vending_machine.rb
+++ vending_machine.rb
@@ -1,5 +1,16 @@
 # Vending Machine
 
+def menu(machine)
+  puts "Welcome to K soft drink machine."
+  index = 0
+  machine.each do |drink, price|
+    puts "#{index += 1}. #{drink} :: $#{price}"
+  end
+  puts "#{index += 1}. Quit"
+  print "Choose the option: "
+  option = gets.chomp.to_i
+end
+
 # Drink name - Cost - Qty in Machine
 machine = [
   ["Coke", 2500, 5],
@@ -8,4 +19,13 @@ machine = [
   ["Tea", 100, 2]
 ]
 
-print machine
+quit = false
+until quit == true
+  option = menu(machine)
+  case option
+  when 1, 2, 3, 4 then compra() 
+  when 5 then quit = true
+  else 
+    puts "Incorrect option, try again!\n\n"
+  end
+end
