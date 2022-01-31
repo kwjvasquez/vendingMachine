224c099d242868fe66c42220c8cfc5710db5d121
Wilman Vasquez <wilman.vasquez@koombea.com>
2022-01-31 16:07:34 -0500

    Reache the amount due in a buy

--- vending_machine.rb
+++ vending_machine.rb
@@ -2,20 +2,40 @@
 
 def menu(machine)
   puts "Welcome to K soft drink machine."
-  index = 0
+  index = 1
   machine.each do |drink, price|
-    puts "#{index += 1}. #{drink} >> $#{price}"
+    puts "#{index}. #{drink} >> $#{price}"
+    index += 1
   end
-  puts "#{index += 1}. Quit"
+  puts "#{index}. Quit"
   print "What do you want? choose the option: "
   option = gets.chomp.to_i
 end
 
-def buy()
+def information(money)
+  puts "\n(INFORMATION) The machine only accept the next bills or coins: "
+  money.each { |value| puts "$#{value}" }
+end
+
+def buy(machine, option)
+  bills_coins_valid = [5000, 2000, 1000, 500, 200, 100]
+  amount_due = machine[option - 1][1] #  product cost. 
+  amount_input = 0
+
+  print "Do you want to see the coins and bills valid? yes (y) any character for no: "
+  info = gets.chomp 
+  information(bills_coins_valid) if info == "y"
+  
+  until amount_input >= amount_due
+    print "Insert bill or coin ($): "
+    input = gets.to_i
+    amount_input += input
+  end
+  puts "Reached the amount\n\n"
 end
 
 # Drink name - Cost - Qty in Machine
-machine = [
+kmachine = [
   ["Coke", 2500, 5],
   ["Water", 2000, 4],
   ["Oatmeal", 1500, 3],
@@ -24,10 +44,10 @@ machine = [
 
 quit = false
 until quit == true
-  option = menu(machine)
+  option = menu(kmachine)
   case option
   when 1, 2, 3, 4
-     buy()
+     buy(kmachine, option)
   when 5  
     quit = true
   else 
