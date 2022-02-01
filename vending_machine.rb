9bed636a4f6c7e0338efc172398e0a5518639f0a
Wilman Vasquez <wilman.vasquez@koombea.com>
2022-02-01 10:41:54 -0500

    Add initial products

---
+++ vending_machine.rb
@@ -0,0 +1,10 @@
+# Vending Machine
+
+products = [
+  {id: 1, name: "Coke", cost: 2_500, qty: 5},
+  {id: 2, name: "Water", cost: 2_000, qty: 4},
+  {id: 3, name: "Oatmeal", cost: 1_500, qty: 3},
+  {id: 4, name: "Tea", cost: 100, qty: 2}
+]
+
+puts products
