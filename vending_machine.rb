ac584a76971f61db636a2e0602ed58d82b036050
Wilman Vasquez <wilman.vasquez@koombea.com>
2022-02-01 13:19:25 -0500

    Show vending machine options

--- vending_machine.rb
+++ vending_machine.rb
@@ -1,5 +1,24 @@
 # Vending Machine
 
+def menu_machine(products)
+  puts "Welcome to K soft drink!"
+  products.each { |product| puts "#{product[:id]}. #{product[:name]} | $#{product[:cost]}"}
+  option_quit = products.size.next
+  puts "#{option_quit}. Quit"
+
+  print "Choose a option: "
+  option = gets.chomp.to_i
+  case option
+  when 1..products.length then buy(products, option)
+  when option_quit then return true
+  else puts "<< Incorrect option, try again >>"
+  end
+end
+
+def buy(products, option)
+  puts "Construction!"
+end
+
 products = [
   {id: 1, name: "Coke", cost: 2_500, qty: 5},
   {id: 2, name: "Water", cost: 2_000, qty: 4},
@@ -7,4 +26,7 @@ products = [
   {id: 4, name: "Tea", cost: 100, qty: 2}
 ]
 
-puts products
+quit = false
+until quit == true
+  quit = menu_machine(products)
+end
